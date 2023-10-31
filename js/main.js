2b033bab91c800979d560caccef0076b086b36c8
serov-k <constantine_serov@vk.com>
2023-11-01 03:28:46 +0500

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,30 +1,6 @@
-import {DESCRIPTIONS, COMMENTS} from './data.js';
-import {getRandomInteger, getUniqueNumber} from './util.js'
+import {getPictures} from './data.js';
+import {renderMiniature} from './renderer.js';
 
-const getComments = () => {
-  const comments = [];
-  for (let i = 0; i <= getRandomInteger(0, 30); i++) {
-    comments.push(COMMENTS[getRandomInteger(0, COMMENTS.length - 1)]);
-  }
-  return comments;
-};
+renderMiniature(getPictures(25));
 
-function creatObject(quantity) {
-
-  const generatedId = getUniqueNumber(1, quantity);
-  const generatedUrl = getUniqueNumber(1, quantity);
-
-  const object = () => ({
-    id: generatedId(),
-    url: `photos/img-${generatedUrl()}.jpg`,
-    description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
-    likes: getRandomInteger(1, 200),
-    comments: getComments(),
-  });
-
-  const arrayObjects = Array.from({length: quantity}, object);
-
-  return arrayObjects;
-}
-
-console.log(creatObject(25));
+console.log(getPictures(25));
---
+++ js/renderer.js
@@ -0,0 +1,34 @@
+const containerMiniatures = document.querySelector('.pictures');
+
+const miniatureSample = document
+    .querySelector('#picture')
+    .content
+    .querySelector('.picture');
+
+const createMiniature = ({url, description, comments, likes}) => {
+
+    const miniature = miniatureSample.cloneNode(true);
+
+    miniature.querySelector('.picture__img').src = url;
+    miniature.querySelector('.picture__img').alt = description;
+    miniature.querySelector('.picture__comments').textContent = comments.length;
+    miniature.querySelector('.picture__likes').textContent = likes;
+
+    return miniature;
+};
+
+const renderMiniature = (pictures) => {
+    
+    const miniatures = document.createDocumentFragment();
+    
+    pictures.forEach((picture) => {
+        const miniature = createMiniature(picture);
+        miniatures.append(miniature);
+    });
+
+    containerMiniatures.append(miniatures);
+};
+
+// console.log(miniatureSample);
+
+export {renderMiniature};
