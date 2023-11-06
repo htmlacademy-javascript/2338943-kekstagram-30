803cee7541a0b73119d54498164220630f6be572
serov-k <constantine_serov@vk.com>
2023-11-07 04:00:42 +0500

    Внёс правки

--- js/renderer.js
+++ js/renderer.js
@@ -1,34 +1,32 @@
 const containerMiniatures = document.querySelector('.pictures');
 
 const miniatureSample = document
-    .querySelector('#picture')
-    .content
-    .querySelector('.picture');
+  .querySelector('#picture')
+  .content
+  .querySelector('.picture');
 
 const createMiniature = ({url, description, comments, likes}) => {
 
-    const miniature = miniatureSample.cloneNode(true);
+  const miniature = miniatureSample.cloneNode(true);
+  
+  miniature.querySelector('.picture__img').src = url;
+  miniature.querySelector('.picture__img').alt = description;
+  miniature.querySelector('.picture__comments').textContent = comments.length;
+  miniature.querySelector('.picture__likes').textContent = likes;
 
-    miniature.querySelector('.picture__img').src = url;
-    miniature.querySelector('.picture__img').alt = description;
-    miniature.querySelector('.picture__comments').textContent = comments.length;
-    miniature.querySelector('.picture__likes').textContent = likes;
-
-    return miniature;
+  return miniature;
 };
 
 const renderMiniature = (pictures) => {
-    
-    const miniatures = document.createDocumentFragment();
-    
-    pictures.forEach((picture) => {
-        const miniature = createMiniature(picture);
-        miniatures.append(miniature);
-    });
+  
+  const miniatures = document.createDocumentFragment();
+  
+  pictures.forEach((picture) => {
+    const miniature = createMiniature(picture);
+    miniatures.append(miniature);
+  });
 
-    containerMiniatures.append(miniatures);
+  containerMiniatures.append(miniatures);
 };
-
-// console.log(miniatureSample);
 
 export {renderMiniature};
